1ddfee09d502c5843bd9037b5b298375f19b020d
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-06 10:05:01 +0100

    Almost done

--- script/AdminOnly.s.sol
+++ script/AdminOnly.s.sol
@@ -7,7 +7,7 @@ contract AdminOlny {
     address public owner;
     uint256 public treasurAmount;
     mapping (address => uint256) public withdrawAllowance;
-    mapping (address => bool) public hasWithdrawn;
+    mapping (address => bool) public allowWithdrawal;
 
     constructor () {
         owner = msg.sender;
@@ -26,6 +26,7 @@ contract AdminOlny {
     function approveWithdraw(address addy, uint256 amount) public onlyOwner payable {
         require(amount < treasurAmount, "Not enough");
         withdrawAllowance[addy] = amount;
+        allowWithdrawal[addy] = true;
     }
 
     function withdrawAmountByUser (uint256 amount) public {
@@ -37,10 +38,25 @@ contract AdminOlny {
             treasurAmount -= amount;
         }
         require(withdrawAllowance[msg.sender] <= amount, "Not enough");
+        require (allowWithdrawal[msg.sender],"Not Allowed");
         withdrawAllowance[msg.sender] -= amount;
         treasurAmount -= amount;
         (bool success,) = msg.sender.call{value:amount}("");
         require(success, "Failed");
+    }
+
+    function resetWithdrawStatus(uint amount, address addy) public onlyOwner{
+         require (allowWithdrawal[msg.sender],"Not Allowed");
+         withdrawAllowance[msg.sender] = 0;
+    }
+
+    function transferOwner (address addy) public onlyOwner {
+        require(addy != address(0),"Not allowed");
+        owner = addy;
+    }
+
+    function checkTreasureAmount () public onlyOwner view returns(uint256) {
+        return treasurAmount;
     }
 
 
